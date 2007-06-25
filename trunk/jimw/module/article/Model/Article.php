e0aa6ad0141fa9820ac558e4e144c6c9858020bc
patou.de.saint.steban <patou.de.saint.steban@0552e26f-3d1b-0410-805e-4fdf74ef7b9c>
2007-06-25 02:09:33 +0000

    [MODULE ARTICLE] Use a Zend_Db_Table for the Model (forgot some new file)

---
+++ trunk/jimw/module/article/Model/Article.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * ArticleController
+ *
+ * @author	    Patou
+ * @category   Jimw_Article
+ * @package    Jimw_Module
+ * @copyright  Copyright (c) 2006-2007 jimw.fr
+ * @license    http://www.jimw.fr
+ * @version    $Id:  $
+ */
+class Article extends Jimw_Db_Table 
+{
+	protected $_name = 'jimw_article';
+}
+?>
