17ff875ef6831c2b47a6b21e1c89ca86fef54b06
Patrice De Saint Steban <patou@users.noreply.github.com>
2007-07-04 00:59:50 +0000

    [DB] Add automaticaly add prefixe column name

--- jimw/lib/Jimw/Db/Table.php
+++ jimw/lib/Jimw/Db/Table.php
@@ -13,15 +13,24 @@ class Jimw_Db_Table extends Zend_Db_Table
 {
 	protected $_rowClass = 'Jimw_Db_Row';
 	protected $_rowsetClass = 'Jimw_Db_Rowset';
+	protected $_tableName = '';
 
 	protected function _setupTableName()
     {
     	$prefix = Zend_Registry::get('db_prefix');
     	parent::_setupTableName();
         if (!strpos($this->_name, $prefix)) {
+        	$this->_tableName = $this->_name;
         	if (!empty($prefix))
         		$this->_name = $prefix . '_' . $this->_name;
         }
+        else {
+        	$this->_tableName = substr($this->_name, strlen($prefix));
+        }
+    }
+    
+    public function getTableName () {
+    	return $this->_tableName;
     }
 }
 ?>
