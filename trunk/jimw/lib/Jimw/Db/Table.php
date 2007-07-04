96a2d2a4bae55dce1939775c229f96f4d2ee2fe0
patou.de.saint.steban <patou.de.saint.steban@0552e26f-3d1b-0410-805e-4fdf74ef7b9c>
2007-07-04 00:04:54 +0000

    [config] Move config file [DB] Now use defined prefix

--- trunk/jimw/lib/Jimw/Db/Table.php
+++ trunk/jimw/lib/Jimw/Db/Table.php
@@ -13,5 +13,15 @@ class Jimw_Db_Table extends Zend_Db_Table
 {
 	protected $_rowClass = 'Jimw_Db_Row';
 	protected $_rowsetClass = 'Jimw_Db_Rowset';
+
+	protected function _setupTableName()
+    {
+    	$prefix = Zend_Registry::get('db_prefix');
+    	parent::_setupTableName();
+        if (!strpos($this->_name, $prefix)) {
+        	if (!empty($prefix))
+        		$this->_name = $prefix . '_' . $this->_name;
+        }
+    }
 }
 ?>
