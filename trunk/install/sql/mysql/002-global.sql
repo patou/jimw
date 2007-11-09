fa04afaab9f57dcb3cdf2a81d035537e2ffc9165
patou.de.saint.steban <patou.de.saint.steban@0552e26f-3d1b-0410-805e-4fdf74ef7b9c>
2007-11-09 19:11:35 +0000

    [UPDATE] Add IF NOT EXISTS in the create JIMW_SCHEMA Table.

--- trunk/install/sql/mysql/002-global.sql
+++ trunk/install/sql/mysql/002-global.sql
@@ -1,5 +1,4 @@
-DROP TABLE IF EXISTS `{PREFIX}schema`;
-CREATE TABLE `{PREFIX}schema` (
+CREATE TABLE `{PREFIX}schema` IF NOT EXISTS (
 `schema_id` INTEGER auto_increment ,
 `schema_module` VARCHAR (32)  NOT NULL default '',
 `schema_version` INTEGER  NOT NULL default '0',
