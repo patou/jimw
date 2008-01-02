f657ac470f3cb3e7d69ec3641e12febbf4728947
Patrice De Saint Steban <patou@users.noreply.github.com>
2008-01-02 13:39:30 +0000

    [RN2007] Fix error with the rcf links

--- jimw/lib/Jimw/Plugin/Site.php
+++ jimw/lib/Jimw/Plugin/Site.php
@@ -12,7 +12,10 @@ class Jimw_Plugin_Site extends Zend_Controller_Plugin_Abstract {
      */
     public function routeStartup(Zend_Controller_Request_Abstract $request)
     {
-    	
+      $front = Zend_Controller_Front::getInstance ();
+      $router = $front->getRouter();
+      $route = new Zend_Controller_Router_Route_Static('?id_local=18', array('alias' => 'accueil'));
+      $router->addRoute('rcf', $route);
     }
 
 }
