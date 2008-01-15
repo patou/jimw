27bb1375e0f5b7ccc6b15f9fc5bfb1c0b26e4c04
Patrice De Saint Steban <patou@users.noreply.github.com>
2008-01-15 22:32:01 +0000

    [Blog] Add Comment controller.

---
+++ jimw/module/blog/Controller/CommentController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * CommentController
+ *
+ * @author	   Patou
+ * @category   Jimw_Blog
+ * @package    Jimw_Module
+ * @copyright  Copyright (c) 2006-2007 jimw.fr
+ * @license    http://www.jimw.fr
+ * @version    $Id$
+ */
+
+include_once('Model/BlogMessage.php');
+include_once('Model/BlogComment.php');
+class Blog_CommentController extends Jimw_Module_Action
+{
+	public function addAction ()
+	{
+		$request = $this->_request;
+		$tree = $request->getTree();
+		$comments = new BlogComment();
+		$comment = $comments->fetchNew();
+		$comment->username = $request->cname;
+		$comment->blogmessage_id = $request->cid;
+		$comment->content = $request->cmessage;
+		$comment->date = new Zend_Db_Expr('NOW()');
+		$comment->save(); 
+		$this->_forward($request->alias, 'blog');
+	}
+}
+?>
