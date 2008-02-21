05baade51e4930b42b4e4c925fd38967614b4491
patou.de.saint.steban <patou.de.saint.steban@0552e26f-3d1b-0410-805e-4fdf74ef7b9c>
2008-02-21 14:12:22 +0000

    Protect add comment

--- trunk/jimw/module/blog/Controller/CommentController.php
+++ trunk/jimw/module/blog/Controller/CommentController.php
@@ -5,7 +5,7 @@
  * @author	   Patou
  * @category   Jimw_Blog
  * @package    Jimw_Module
- * @copyright  Copyright (c) 2006-2007 jimw.fr
+ * @copyright  Copyright (c) 2006-2008 jimw.fr
  * @license    http://www.jimw.fr
  * @version    $Id$
  */
@@ -14,18 +14,23 @@ include_once('Model/BlogMessage.php');
 include_once('Model/BlogComment.php');
 class Blog_CommentController extends Jimw_Module_Action
 {
-	public function addAction ()
-	{
-		$request = $this->_request;
-		$tree = $request->getTree();
-		$comments = new BlogComment();
-		$comment = $comments->fetchNew();
-		$comment->username = $request->cname;
-		$comment->blogmessage_id = $request->cid;
-		$comment->content = $request->cmessage;
-		$comment->date = new Zend_Db_Expr('NOW()');
-		$comment->save(); 
-		$this->_forward($request->alias, 'blog');
-	}
+  public function addAction ()
+  {
+    $request = $this->_request;
+    $tree = $request->getTree();
+    if ($tree->param->comment) {
+      $comments = new BlogComment();
+      $comment = $comments->fetchNew();
+      $comment->username = $request->cname;
+      $comment->blogmessage_id = $request->cid;
+      $comment->content = $request->cmessage;
+      $comment->date = new Zend_Db_Expr('NOW()');
+      $comment->save();
+    }
+    else {
+      throw new Jimw_Exception('Comment is not allowed');
+    }
+    $this->_forward($request->alias, 'blog');
+  }
 }
 ?>
