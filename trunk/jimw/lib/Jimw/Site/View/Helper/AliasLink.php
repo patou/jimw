db6d9ba0d227a5469fa163b2d47c29015aa9fe19
logisim <logisim@0552e26f-3d1b-0410-805e-4fdf74ef7b9c>
2008-03-03 17:18:26 +0000

    [Jimw_Site_View] Helpers

---
+++ trunk/jimw/lib/Jimw/Site/View/Helper/AliasLink.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Jimw_Site_View_Helper_AliasLink
+ *
+ * @author	   Logisim
+ * @category   Jimw_Core
+ * @package    Jimw
+ * @copyright  Copyright (c) 2006-2008 jimw.fr
+ * @license    http://www.jimw.fr
+ * @version    $Id$
+ */
+
+class Jimw_Site_View_Helper_AliasLink
+{
+	
+	public function aliasLink($text) {
+ 
+        $tree = new Jimw_Site_Tree();
+        $urlHelper = new Jimw_Site_View_Helper_Url();
+        $result = $text;
+        
+        // Without text
+        $count = preg_match_all('/\[\[([^\[\]\s]+)\]\]/', $text, $matches);
+        for ($i=0; $i<$count; $i++) {
+			$page = $tree->fetchRow(array('tree_alias = ?' => $matches[1][$i]));
+        	if ($page) {
+        		$url = $urlHelper->url(array('alias' => $matches[1][$i]), 'alias', true);
+        		$result = str_replace($matches[0][$i], '<a href="'.$url.'">'.$page->pagetitle.'</a>', $result);
+        	} else {
+				$result = str_replace($matches[0][$i], '<a href="#">???</a>', $result);
+			}
+        }
+
+        // With text
+        $count = preg_match_all('/\[\[([^\[\]\s]+)\s([^\[\]]+)\]\]/', $text, $matches);
+        for ($i=0; $i<$count; $i++) {
+        	//$url = $urlHelper->url(array('alias' => $matches[1][$i]), 'alias', true);
+			$page = $tree->fetchRow(array('tree_alias = ?' => $matches[1][$i]));
+        	if ($page) {
+        		$url = $urlHelper->url(array('alias' => $matches[1][$i]), 'alias', true);
+        		$result = str_replace($matches[0][$i], '<a href="'.$url.'">'.$matches[2][$i].'</a>', $result);
+        	} else {
+				$result = str_replace($matches[0][$i], '<a href="#">???</a>', $result);
+			}
+        }
+
+        return $result;
+	}
+	
+}
