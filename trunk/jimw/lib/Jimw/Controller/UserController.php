8cdbdc3352a88bd45b7b2d85fef4ad35f66b41e3
logisim <logisim@0552e26f-3d1b-0410-805e-4fdf74ef7b9c>
2008-03-19 23:35:09 +0000

    [User] Front controller

---
+++ trunk/jimw/lib/Jimw/Controller/UserController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * UserController
+ *
+ * @author	   Logisim
+ * @category   Jimw_Core
+ * @package    Jimw
+ * @copyright  Copyright (c) 2006-2008 jimw.fr
+ * @license    http://www.jimw.fr
+ * @version    $Id$
+ */
+class UserController extends Jimw_Module_Action {
+	
+	public function subscribeAction()
+    {
+		echo "Test subscribe";
+		//$this->signup();
+    }
+    
+}
+ ?>
