cb1229134f89f24c8cf4d2420308d0c7052a0e05
logisim <logisim@0552e26f-3d1b-0410-805e-4fdf74ef7b9c>
2008-07-08 09:19:12 +0000

    [Jimw_Site_View_Helper] Adding and correcting view helpers

--- trunk/jimw/lib/Jimw/Site/View/Helper/Troncate.php
+++ trunk/jimw/lib/Jimw/Site/View/Helper/Troncate.php
@@ -1,9 +1,19 @@
 <?php
+/**
+ * Jimw_Site_View_Helper_Troncate
+ *
+ * @author	   Patou
+ * @category   Jimw_Core
+ * @package    Jimw
+ * @copyright  Copyright (c) 2006-2008 jimw.fr
+ * @license    http://www.jimw.fr
+ * @version    $Id$
+ */
 
 class Jimw_Site_View_Helper_Troncate {
 	public function troncate($message, $length = 50) {
 		if (strlen($message) > $length) {
-			return substr($message, 0, $length) . ' ...';
+			return substr($message, 0, $length) . '...';
 		}
 		return $message;
 	}
