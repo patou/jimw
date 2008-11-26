f8a76affffee6a8e2d70c937512af7754d81e303
logisim <logisim@0552e26f-3d1b-0410-805e-4fdf74ef7b9c>
2008-11-26 17:56:50 +0000

    [Shop] Updating MySQL script

--- trunk/jimw/module/shop/install/sql/mysql/001-shop.sql
+++ trunk/jimw/module/shop/install/sql/mysql/001-shop.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS `{PREFIX}shopalbum` (
 `shopalbum_artist` VARCHAR( 64 ) NOT NULL ,
 `shopalbum_editor` VARCHAR( 64 ) NOT NULL ,
 `shopalbum_year` CHAR( 4 ) NOT NULL ,
+`shopalbum_image` VARCHAR( 255 ) NOT NULL ,
 `shopalbum_musicfile` VARCHAR( 128 ) NOT NULL ,
 `shopalbum_scorefile` VARCHAR( 128 ) NOT NULL ,
 `shopalbum_musicprice` DECIMAL( 5, 2 ) NOT NULL ,
@@ -27,4 +28,17 @@ CREATE TABLE IF NOT EXISTS `{PREFIX}shopsong` (
 `shopsong_scorepreview` VARCHAR( 128 ) NOT NULL ,
 `shopsong_scoreprice` DECIMAL( 5, 2 ) NOT NULL ,
 INDEX ( `shopalbum_id` )
+);
+
+CREATE TABLE IF NOT EXISTS `{PREFIX}shopsongorder` (
+`shopsongorder_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`shopsongorder_name` varchar(255) NOT NULL default '',
+`shopsongorder_firstname` varchar(255) NOT NULL default '',
+`shopsongorder_email` varchar(255) NOT NULL default '',
+`shopsongorder_date` datetime NOT NULL default '0000-00-00 00:00:00',
+`shopsongorder_musiclist` text NOT NULL,
+`shopsongorder_scorelist` text NOT NULL,
+`shopsongorder_price` decimal(7,2) NOT NULL default '0.00',
+`shopsongorder_paid` char(1) NOT NULL default '',
+`shopsongorder_key` varchar(32) NOT NULL default ''
 );
