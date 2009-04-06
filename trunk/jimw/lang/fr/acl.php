140e91390b3b1d6c555a5221e6fd75af43f308d8
logisim <logisim@0552e26f-3d1b-0410-805e-4fdf74ef7b9c>
2009-04-06 17:36:32 +0000

    [Article] Adding aliasLink helper and toolbar CSS support [Lang] Correction all french language file

--- trunk/jimw/lang/fr/acl.php
+++ trunk/jimw/lang/fr/acl.php
@@ -2,18 +2,18 @@
 return array(
     // ACL
     'Administrator' => 'Administrateur',
-    'Members' => 'Utilisateurs',
-    'Gest' => 'Invité',
+    'Member' => 'Membre',
+    'Guest' => 'Invité',
     'All' => 'Tous',
     'Allow' => 'Autoriser',
     'Deny' => 'Refuser',
-    'Default' => 'Défaut',
-    'Public website' => 'Site Publique',
+    'Default' => 'Par défaut',
+    'Public website' => 'Site public',
     'Administrator website' => 'Site d\'administration',
     'Access to the website' => 'Accéder au site',
-    'Can login to this website' => 'Peux se connecter sur le site',
+    'Can login to this website' => 'Peut se connecter sur le site',
     'Access to the administration' => 'Accéder à l\'administration',
-    'System Administration' => 'Administration du système',
-    'File Administration' => 'Administration des fichiers',
-    "You did't have the permission to access to this section of the site" => "Vous n'avez pas la permission d'accèder à cette section du site"
+    'System administration' => 'Administration du système',
+    'File administration' => 'Administration des fichiers',
+    "You don't have permission to access this section of the site" => "Vous n'avez pas la permission d'accéder à cette partie du site"
 );
