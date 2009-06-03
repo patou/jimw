40eebfb6fb08e248ffa8770aa9797f78bcfe8038
Patrice De Saint Steban <patou@users.noreply.github.com>
2009-06-03 21:43:31 +0000

    Fckeditor youtube pluging

---
+++ admin/public/editor/plugins/youtube/lang/fr.js
@@ -0,0 +1,21 @@
+/*
+ * For FCKeditor 2.3
+ * 
+ * 
+ * File Name: fr.js
+ * 	French language file for the youtube plugin.
+ * 
+ * File Authors:
+ * 		Patrice De Saint Steban
+ */
+
+FCKLang['YouTubeTip']			= 'Insérer/Modifier une vidéo YouTube' ;
+FCKLang['DlgYouTubeTitle']		= 'YouTube Vidéo propriété' ;
+FCKLang['DlgYouTubeCode']		= '"Merci d\'insérer l\'url de la vidéo youtube."' ;
+FCKLang['DlgYouTubeSecurity']	= 'URL invalide.' ;
+FCKLang['DlgYouTubeURL']	    = 'URL' ;
+FCKLang['DlgYouTubeWidth']	    = 'Largeur' ;
+FCKLang['DlgYouTubeHeight']	    = 'Hauteur' ;
+FCKLang['DlgYouTubeQuality']    = 'Qualité' ;
+FCKLang['DlgYouTubeLow']	    = 'Basse' ;
+FCKLang['DlgYouTubeHigh']	    = 'Haute (Si disponible)' ;
