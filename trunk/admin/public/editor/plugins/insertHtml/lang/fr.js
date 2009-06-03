d195e3a834280c64d7ef0417d8f73ead27e62ff8
patou.de.saint.steban <patou.de.saint.steban@0552e26f-3d1b-0410-805e-4fdf74ef7b9c>
2009-06-03 21:42:54 +0000

    Fckeditor insertHtml pluging

---
+++ trunk/admin/public/editor/plugins/insertHtml/lang/fr.js
@@ -0,0 +1,6 @@
+
+
+	FCKLang.inserHTML_buttonTooltip					= 'Inserer HTML';
+	FCKLang.insertHtml_dialogTitle						= 'Inserer HTML';
+	FCKLang.inserHtml_help								= 'Entrez votre code HTML ci-dessous et cliquez sur \'ok\' pour l\'inserer à la position du curseur dans l\'editeur.';
+
