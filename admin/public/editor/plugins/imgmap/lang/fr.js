4605221299a462e07aa0b1e337aba45ba76a295b
Patrice De Saint Steban <patou@users.noreply.github.com>
2009-06-03 21:39:58 +0000

    Fckeditor imgmap pluging

---
+++ admin/public/editor/plugins/imgmap/lang/fr.js
@@ -0,0 +1,24 @@
+
+
+FCKLang.imgmapBtn			= 'Insérer/Editer Image Map';
+FCKLang.imgmapBtnRemove			= 'Supprimer Image Map';
+FCKLang.imgmapDlgTitle		= 'Image Map Editeur';
+FCKLang.imgmapDlgName		= 'ImageMapEditeur';
+
+FCKLang.msgImageNotSelected = 'Vous devez selectionnez une image avant d\'ouvrir l\'editeur d\'image Map' ;
+
+FCKLang.imgmapNoLabel = 'Sans label' ;
+FCKLang.imgmapLabelNumber = 'Labelisé par l\'identifiant numérique' ;
+FCKLang.imgmapLabelAlt = 'Labelisé par la balise alt' ;
+FCKLang.imgmapLabelHref = 'Labelisé par le lien' ;
+FCKLang.imgmapLabelTitle = 'Labelisé par le titre' ;
+FCKLang.imgmapLabelCoords = 'Labelisé par les coordonnées' ;
+
+FCKLang.imgmapMap = 'Map' ;
+FCKLang.imgmapMapName = 'Nom de la Map' ;
+FCKLang.imgmapMapAreas = 'Image Map Areas' ;
+
+FCKLang.imgmapPointer = 'Pointeur' ;
+FCKLang.imgmapRectangle = 'Rectangle' ;
+FCKLang.imgmapCircle = 'Cercle' ;
+FCKLang.imgmapPolygon = 'Polygone' ;
