2dde97e31f0610ef9512323ea8cdc1b989aa282f
Patrice De Saint Steban <patou@users.noreply.github.com>
2009-09-03 10:25:50 +0000

    Correct the categoryBlog Rss et Js format

--- jimw/module/categoryblog/views/scripts/blog/blog.js
+++ jimw/module/categoryblog/views/scripts/blog/blog.js
@@ -6,10 +6,9 @@ if (count($this->messages) > 0) {
   echo "document.write ('<ul>');\n";
   //$url = $this->url(array('alias' => $this->tree->alias, 'ext' => 'phtml'), 'alias', true);
   foreach ($this->messages as $message) {
-    $tree = new Jimw_Site_Tree();
-    $result = $tree->find($message->tree_parentid)->current();
-    $url = $this->url(array('alias' => $result->alias, 'ext' => 'phtml'), 'alias', true, true);
-    echo "document.write ('<li><a href=\"".$url."#".$message->id."\">".str_replace("'","\'",$message->title)."</a></li>');\n";
+    //$url = $this->url(array('alias' => $message->tree_alias, 'ext' => 'phtml'), 'alias', true, true);
+	  $url = $this->url(array('module' => 'blog', 'controller' => 'message', 'action' => 'view', 'format' => 'phtml', 'alias' => $message->tree_alias, 'mid' => $message->id), 'format');
+    echo "document.write ('<li><a href=\"".$url."\">".str_replace("'","\'",$message->title)."</a></li>');\n";
   }
   echo "document.write ('</ul>');\n";
 }
