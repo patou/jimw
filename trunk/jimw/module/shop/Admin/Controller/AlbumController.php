e86dacfa205d0f6cc97f87fcd31c1a9ca1fc65da
logisim <logisim@0552e26f-3d1b-0410-805e-4fdf74ef7b9c>
2009-10-17 11:58:38 +0000

    [Shop] Adding album list to admin panel

---
+++ trunk/jimw/module/shop/Admin/Controller/AlbumController.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * indexController.php
+ *
+ * @author	   Logisim
+ * @category   Blog_Admin
+ * @package    Jimw_Module
+ * @copyright  Copyright (c) 2006-2007 jimw.fr
+ * @license    http://www.jimw.fr
+ * @version    $Id: MessageController.php 352 2009-09-06 18:09:49Z patou.de.saint.steban $
+ */
+include_once (dirname(__FILE__) . '/../../Controller/Model/ShopAlbum.php');
+include_once (dirname(__FILE__) . '/../../Controller/Model/ShopSong.php');
+include_once (dirname(__FILE__) . '/../Form/AlbumForm.php');
+class Shop_AlbumController extends Jimw_Admin_Action
+{
+	public function listAction () {
+		$albummodel = new ShopAlbum();
+		$request = $this->getRequest();
+		$id = $request->getParam('id');
+		$result = Zend_Paginator::factory($albummodel->select()->where('tree_id = ?', $id)->order('shopalbum_id'));
+
+		// initialisation des valeurs par défaut
+		$result->setPageRange(10);
+		$result->setItemCountPerPage(10); //TODO $this->module->config->nbPerPage
+		$page = 1;
+		if (isset($request->page))
+		    $page = $request->page;
+		$result->setCurrentPageNumber($page);
+
+		$this->view->albums_list = $result;
+		$this->view->tree_id = $id;
+	}
+
+	public function editAction () {
+		$request = $this->getRequest();
+		$id = $request->blogmessage_id;
+		$form = new MessageForm();
+		$form->addSubmit('Save');
+		$messages = new BlogMessage();
+		$message = $messages->fetchRow(array('blogmessage_id = ?' => $id));
+		if (!$message)
+			throw new Jimw_Admin_Exception('Message doesn\'t exist');
+		if ($request->isPost() && $form->isValid($request->getPost())) {
+            $values = $form->getValues();
+            $message->header = $values['blogmessage_header'];
+			$message->title = $values['blogmessage_title'];
+			$message->content = $values['blogmessage_content'];
+			$message->date = $values['blogmessage_date'];
+			$message->comment = $values['blogmessage_comment'];
+			$message->tree_id = $values['tree_id'];
+			$message->save ();
+			$this->_helper->getHelper('FlashMessenger')->addMessage ('Save successful');
+			$this->_redirect($this->view->url(array('action'=>'edit', 'controller' => 'tree', 'module' => 'default', 'id' => $message->tree_parentid), 'format', true), array('prependBase' => false));
+		}
+		else {
+		    if (!$request->isPost()) {
+		        $form->populate($message->toArray());
+		    }
+		    $this->view->message = $message;
+		    $this->view->form = $form;
+		    $this->render('form');
+		}
+	}
+/**
+	public function saveAction () {
+		$request = $this->getRequest();
+		$id = $request->id;
+		$messages = new BlogMessage();
+		if (is_numeric($id)) {
+		   $message = $messages->fetchRow(array('blogmessage_id = ?' => $id));
+		} else {
+		   $message = false;
+		}
+		if (!$message) {
+			$message = $messages->fetchNew();
+			$message->tree_parentid = $request->treeparentid;
+			$message->header = $request->header;
+			$message->title = $request->title;
+			$message->content = $request->content;
+			$message->date = $request->date;
+			$message->tree_id = $request->treeid;
+			$message->user_id = 0;
+			$message->save();
+		} else {
+			$message->header = $request->header;
+			$message->tree_parentid = $request->treeparentid;
+			$message->title = $request->title;
+			$message->content = $request->content;
+			$message->date = $request->date;
+			$message->tree_id = $request->treeid;
+			$message->user_id = 0;
+			$message->save ();
+		}
+		$this->_helper->getHelper('FlashMessenger')->addMessage ('Save successful');
+		$this->_redirect($this->view->url(array('action'=>'edit', 'controller' => 'tree', 'module' => 'default', 'id' => $message->tree_parentid), 'format', true), array('prependBase' => false));
+	}
+
+	public function insertAction () {
+		$request = $this->getRequest();
+		$id = $request->id;
+		$messages = new BlogMessage();
+		$message = $messages->fetchNew();
+		$message->tree_parentid = $request->treeparentid;
+		$message->header = $request->header;
+		$message->title = $request->title;
+		$message->content = $request->content;
+		$message->date = $request->date;
+		$message->tree_id = $request->treeid;
+		$message->comment = true;
+		Jimw_Debug::dump($message);
+		$message->user_id = 0;
+		$message->save();
+		$this->_helper->getHelper('FlashMessenger')->addMessage ('Insert successful');
+		$this->_redirect($this->view->url(array('action'=>'edit', 'controller' => 'tree', 'module' => 'default', 'id' => $message->tree_parentid), 'format', true), array('prependBase' => false));
+	}*/
+
+	public function addAction () {
+	    $request = $this->getRequest();
+		$id = $request->message_id;
+		$form = new MessageForm();
+		$form->addSubmit('Save');
+		$messages = new BlogMessage();
+		$message = $messages->fetchNew();
+		$message->tree_parentid = $request->getParam('tree_parentid');
+		$message->date = $message->getFormatedDate(Zend_Date::now());
+		if ($request->isPost() && $form->isValid($request->getPost())) {
+            $values = $form->getValues();
+            $message->header = $values['blogmessage_header'];
+			$message->title = $values['blogmessage_title'];
+			$message->content = $values['blogmessage_content'];
+			$message->comment = $values['blogmessage_comment'];
+			$message->date = $values['blogmessage_date'];
+			$message->tree_id = $values['tree_id'];
+			$user = $this->getUser();
+			$message->user_id = $user->user_id;
+			$message->save ();
+			$this->_helper->getHelper('FlashMessenger')->addMessage ('Save successful');
+			$this->_redirect($this->view->url(array('action'=>'edit', 'controller' => 'tree', 'module' => 'default', 'id' => $message->tree_parentid), 'format', true), array('prependBase' => false));
+		}
+		else {
+		    if (!$request->isPost()) {
+		        $message->comment = 1;
+		        $form->populate($message->toArray());
+		    }
+		    $this->view->form = $form;
+		    $this->render('form');
+		}
+	}
+
+	public function deleteAction () {
+		$id = $this->getRequest()->blogmessage_id;
+		$blogs = new BlogMessage();
+		$blog = $blogs->find($id);
+		if (!count($blog)) {
+			throw new Jimw_Admin_Exception('Message doesn\'t exist');
+
+		}
+		$blog = $blog->current();
+		$blogid = $blog->tree_parentid;
+		$blog->delete ();
+		$this->_helper->getHelper('FlashMessenger')->addMessage ('Delete successful');
+		$this->_redirect($this->view->url(array('action'=>'edit', 'controller' => 'tree', 'module' => 'default', 'id' => $message->tree_parentid), 'format', true), array('prependBase' => false));
+	}
+}
+?>
