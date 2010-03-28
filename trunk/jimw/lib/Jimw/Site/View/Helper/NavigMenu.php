89b3baa468d02001984dd02716dad1c410d462af
patou.de.saint.steban <patou.de.saint.steban@0552e26f-3d1b-0410-805e-4fdf74ef7b9c>
2010-03-28 21:50:42 +0000

    Add prepend and append html options to the navigMenu helper

--- trunk/jimw/lib/Jimw/Site/View/Helper/NavigMenu.php
+++ trunk/jimw/lib/Jimw/Site/View/Helper/NavigMenu.php
@@ -19,6 +19,17 @@ class Jimw_View_Helper_NavigMenu extends Zend_View_Helper_Navigation_Menu
      * @var bool
      */
     protected $_expandBranch = false;
+    
+    /**
+     * Html added before the text in the link
+     * @var string
+     */
+    protected $_prependHtml = '';
+    /**
+     * Html added after the text in the link
+     * @var string
+     */
+    protected $_appendHtml = '';
 
     /**
      * View helper entry point:
@@ -29,7 +40,8 @@ class Jimw_View_Helper_NavigMenu extends Zend_View_Helper_Navigation_Menu
      * @return Zend_View_Helper_Navigation_Menu      fluent interface,
      *                                               returns self
      */
-    public function navigMenu(Zend_Navigation_Container $container = null)
+
+	public function navigMenu(Zend_Navigation_Container $container = null)
     {
         if (null !== $container) {
             $this->setContainer($container);
@@ -202,6 +214,54 @@ class Jimw_View_Helper_NavigMenu extends Zend_View_Helper_Navigation_Menu
 
         return $html;
     }
+    
+    /**
+     * Returns an HTML string containing an 'a' element for the given page if
+     * the page's href is not empty, and a 'span' element if it is empty
+     *
+     * Overrides {@link Zend_View_Helper_Navigation_Abstract::htmlify()}.
+     *
+     * @param  Zend_Navigation_Page $page  page to generate HTML for
+     * @return string                      HTML string for the given page
+     */
+    public function htmlify(Zend_Navigation_Page $page)
+    {
+        // get label and title for translating
+        $label = $page->getLabel();
+        $title = $page->getTitle();
+
+        // translate label and title?
+        if ($this->getUseTranslator() && $t = $this->getTranslator()) {
+            if (is_string($label) && !empty($label)) {
+                $label = $t->translate($label);
+            }
+            if (is_string($title) && !empty($title)) {
+                $title = $t->translate($title);
+            }
+        }
+
+        // get attribs for element
+        $attribs = array(
+            'id'     => $page->getId(),
+            'title'  => $title,
+            'class'  => $page->getClass()
+        );
+
+        // does page have a href?
+        if ($href = $page->getHref()) {
+            $element = 'a';
+            $attribs['href'] = $href;
+            $attribs['target'] = $page->getTarget();
+        } else {
+            $element = 'span';
+        }
+
+        return '<' . $element . $this->_htmlAttribs($attribs) . '>'
+        	 . $this->_prependHtml
+             . $this->view->escape($label)
+             . $this->_appendHtml
+             . '</' . $element . '>';
+    }
 
     /**
      * Renders helper
@@ -248,4 +308,43 @@ class Jimw_View_Helper_NavigMenu extends Zend_View_Helper_Navigation_Menu
 
         return $html;
     }
+    
+    protected function _normalizeOptions(array $options = array())
+    {
+     	if (isset($options['prependHtml'])) {
+            $this->setPrependHtml($options['prependHtml']);
+        }
+    	if (isset($options['appendHtml'])) {
+            $this->setAppendHtml($options['appendHtml']);
+        }
+    	return parent::_normalizeOptions($options);
+    }
+    
+    /**
+	 * @return the $prependHtml
+	 */
+	public function getPrependHtml() {
+		return $this->_prependHtml;
+	}
+
+	/**
+	 * @return the $appendHtml
+	 */
+	public function getAppendHtml() {
+		return $this->_appendHtml;
+	}
+
+	/**
+	 * @param $prependHtml the $prependHtml to set
+	 */
+	public function setPrependHtml($prependHtml) {
+		$this->_prependHtml = $prependHtml;
+	}
+
+	/**
+	 * @param $appendHtml the $appendHtml to set
+	 */
+	public function setAppendHtml($appendHtml) {
+		$this->_appendHtml = $appendHtml;
+	}
 }
