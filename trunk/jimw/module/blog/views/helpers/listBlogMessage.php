714f4ee87e3344279160427ee3bb4b028a07a635
patou.de.saint.steban <patou.de.saint.steban@0552e26f-3d1b-0410-805e-4fdf74ef7b9c>
2010-09-18 22:45:59 +0000

    [Blog] Add options to display or hide the author and the date Add a helper to display a list of messages links

---
+++ trunk/jimw/module/blog/views/helpers/listBlogMessage.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Blog_View_Helper_ListBlogMessage
+ *
+ * @author	    Patou
+ * @category   Jimw_Core
+ * @package    Jimw
+ * @copyright  Copyright (c) 2006-2007 jimw.fr
+ * @license    http://www.jimw.fr
+ * @version    $Id$
+ */
+
+include_once(dirname(__FILE__) . '/../../Controller/Model/BlogMessage.php');
+include_once(dirname(__FILE__) . '/../../Controller/Model/BlogComment.php');
+include_once(dirname(__FILE__) . '/../../Controller/Model/BlogCommentForm.php');
+
+class Blog_View_Helper_ListBlogMessage extends Zend_View_Helper_Abstract {
+
+    /**
+     * Display the menu
+     *
+     * @param string $name
+     * @param boolean $display Auto render the menu with <li></li> tag
+     * @return string|Jimw_Site_Tree_Row
+     */
+    public function listBlogMessage($tree_alias, $options = array('number' => 5)) {
+        $messagetable = new BlogMessage();
+        $treetable = new Jimw_Site_Tree();
+
+        $tree = $treetable->findAlias($tree_alias)->current();
+
+        $list = $messagetable->fetchAll($messagetable->select()->where('tree_parentid = ?', $tree->id)
+                                ->where('blogmessage_date <= NOW()')
+                                ->order('blogmessage_date DESC')
+                                ->limit($options['number']));
+        $html = '<ul>';
+        //$url = $this->url(array('alias' => $this->tree->alias, 'ext' => 'phtml'), 'alias', true);
+        foreach ($list as $message) {
+            $result = $treetable->find($message->tree_parentid)->current();
+            $url = $this->view->url(array('alias' => $result->alias, 'ext' => 'phtml'), 'alias', true, true);
+            $html .= '<li><a href="' . $url . '#' . $message->id . '">' . $message->title . '</a></li>';
+        }
+        $html .= '</ul>';
+        return $html;
+    }
+
+}
