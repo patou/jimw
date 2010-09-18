edef9f403f3959dce12c9715ec4203e438f47cdc
Patrice De Saint Steban <patou@users.noreply.github.com>
2010-09-18 12:47:32 +0000

    Correct Bugs when the edit action in the ManageControler forward to an other action and controller. (the action() view helper didn't redirect or forward) Must configure in the jimw.xml of the module to set the action and controller for the edit page.
    Exemple :
        <controllers><!-- List of controller can be used by a tree -->
            <shop><!-- Name of the controller -->
                <name>Blog</name><!-- Readable name -->
                <action>list</action>
                <controler>album</controler>
            </shop>
        </controllers>

--- jimw/lib/Jimw/Admin/Controller/TreeController.php
+++ jimw/lib/Jimw/Admin/Controller/TreeController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * TreeController.php
  *
@@ -9,21 +10,20 @@
  * @license    http://www.jimw.fr
  * @version    $Id$
  */
-class TreeController extends Jimw_Admin_Action
-{
-    public function indexAction ()
-    {
+class TreeController extends Jimw_Admin_Action {
+
+    public function indexAction() {
         //if (!$this->_helper->getHelper('FlashMessenger')->hasMessages())
-            $this->_forward('list');
-        /*else
-            $this->view->flashmessenger = $this->_helper->getHelper('FlashMessenger')->getCurrentMessages();*/
+        $this->_forward('list');
+        /* else
+          $this->view->flashmessenger = $this->_helper->getHelper('FlashMessenger')->getCurrentMessages(); */
     }
+
     /**
      * List all the page
      *
      */
-    public function listAction ()
-    {
+    public function listAction() {
         $tree = new Jimw_Site_Tree();
         $site = Zend_Registry::get('site');
         $parentid = $tree->find($site->tree_id);
@@ -32,25 +32,23 @@ class TreeController extends Jimw_Admin_Action
         $this->view->list_tree = $parentid;
         $this->view->flashmessenger = $this->_helper->getHelper('FlashMessenger')->getCurrentMessages();
     }
-    
+
     /**
      * Display the trash list of page
      *
      */
-    public function trashAction ()
-    {
+    public function trashAction() {
         $tree = new Jimw_Site_Tree();
         $this->view->list_tree = $tree->fetchAll($tree->select()->where('tree_status < 0'));
         $this->view->flashmessenger = $this->_helper->getHelper('FlashMessenger')->getCurrentMessages();
         $this->render('list');
     }
-    
+
     /**
      * Edit a page
      *
      */
-    public function editAction ()
-    {
+    public function editAction() {
         $req = $this->getRequest();
         $id = $req->getParam('id');
         if (empty($id))
@@ -59,11 +57,12 @@ class TreeController extends Jimw_Admin_Action
         $form = new Jimw_Admin_Form_TreeForm();
         $form->getElement('module_path')->setAttrib('disable', true)->setRequired(false);
         $result = $tree->fetchAll($tree->select()->where('tree_id = ?', $id));
-        if (! count($result)) {
+        if (!count($result)) {
             throw new Jimw_Admin_Exception("The tree $id doesn't exist");
         }
         $result = $result->current();
-        $this->_addModuleConfig($result, $form);
+        $module = $result->findParentJimw_Site_Module();
+        $this->_addModuleConfig($module, $form);
         if ($req->isPost() && $form->isValid($req->getPost())) {
             $values = $form->getValues();
             $result->pagetitle = $values['tree_pagetitle'];
@@ -107,19 +106,21 @@ class TreeController extends Jimw_Admin_Action
         $this->view->form = $form;
         $this->view->id = $id;
         if ($result->status >= 0) {
+            $action = (isset($module->xml->controllers->{$module->path}->action)) ? $module->xml->controllers->{$module->path}->action : 'edit';
+            $controller = (isset($module->xml->controllers->{$module->path}->controler)) ? $module->xml->controllers->{$module->path}->controler : 'manage';
+            Jimw_Debug::display($action . ':' . $controller);
             //$this->_helper->actionStack('edit', 'manage', $result->module_path, array('tree' => $result, 'id' => $id, 'alias' => $result->alias));
-            $this->view->edit_form = $this->view->action('edit', 'manage', $result->module_path, array('tree' => $result, 'id' => $id, 'alias' => $result->alias));
+            $this->view->edit_form = $this->view->action($action, $controller, $result->module_path, array('tree' => $result, 'id' => $id, 'alias' => $result->alias));
             //$this->_forward('edit', 'manage', $result->module_path);
         }
         $this->render('form');
     }
-    
+
     /**
      * Create a new page
      *
      */
-    public function addAction ()
-    {
+    public function addAction() {
         $req = $this->getRequest();
         $tree = new Jimw_Site_Tree();
         $site = Zend_Registry::get('site');
@@ -160,18 +161,17 @@ class TreeController extends Jimw_Admin_Action
             $parent = $tree->find($values['tree_parentid']);
             if (isset($parent) && count($parent)) {
                 $save->site_id = $parent->current()->site_id;
-            }
-            else {
+            } else {
                 $save->site_id = $site->id;
             }
             $save->save();
             $this->_helper->getHelper('FlashMessenger')->addMessage($this->_('Insert successful') . $req->tree_pagetitle);
-            $this->_redirect($this->view->url(array('action'=>'edit', 'controller' => 'tree', 'module' => 'default', 'id' => $id), 'format', true), array('prependBase' => false));
+            $this->_redirect($this->view->url(array('action' => 'edit', 'controller' => 'tree', 'module' => 'default', 'id' => $id), 'format', true), array('prependBase' => false));
         } else {
             $this->view->tree = $tree->fetchNew();
-            /*$module = new Jimw_Site_Module();
-			$this->view->modules = $module->fetchAll();
-			$this->view->form_type = 'insert';*/
+            /* $module = new Jimw_Site_Module();
+              $this->view->modules = $module->fetchAll();
+              $this->view->form_type = 'insert'; */
             $form->addSubmit('Insert');
             $form->populate(array('tree_status' => Jimw_Site_Tree::PUBLISHED, 'site_id' => $site->id));
             $this->view->form = $form;
@@ -179,16 +179,16 @@ class TreeController extends Jimw_Admin_Action
             $this->render('form');
         }
     }
+
     /**
      * Delete a page : Move it to the trash or delete it from the database if the page are in the trash
      *
      */
-    public function deleteAction ()
-    {
+    public function deleteAction() {
         $id = $this->_request->id;
         $tree = new Jimw_Site_Tree();
         $result = $tree->fetchAll($tree->select()->where('tree_id = ?', $id));
-        if (! count($result)) {
+        if (!count($result)) {
             throw new Jimw_Admin_Exception("This tree doesn't exist");
         }
         $result = $result->current();
@@ -198,25 +198,23 @@ class TreeController extends Jimw_Admin_Action
             //TODO Delete the arborescence !
             $this->_helper->getHelper('FlashMessenger')->addMessage('Delete successful');
             $this->_forward('delete', 'manage', $module_path, array('tree' => $result, 'id' => $id, 'alias' => $tree->alias));
-        }
-        else {
+        } else {
             $result->status = Jimw_Site_Tree::DELETED;
             $result->save();
             $this->_helper->getHelper('FlashMessenger')->addMessage('Move to the trash');
             $this->_forward('list');
         }
     }
-    
+
     /**
      * Move out from the trash
      *
      */
-	public function restoreAction ()
-    {
+    public function restoreAction() {
         $id = $this->_request->id;
         $tree = new Jimw_Site_Tree();
         $result = $tree->fetchAll($tree->select()->where('tree_id = ?', $id));
-        if (! count($result)) {
+        if (!count($result)) {
             throw new Jimw_Admin_Exception("This tree doesn't exist");
         }
         $result = $result->current();
@@ -226,17 +224,16 @@ class TreeController extends Jimw_Admin_Action
         $this->_helper->getHelper('FlashMessenger')->addMessage('Restore successful');
         $this->_forward('list');
     }
-    
+
     /**
      * Publish a page
      *
      */
-	public function publishAction ()
-    {
+    public function publishAction() {
         $id = $this->_request->id;
         $tree = new Jimw_Site_Tree();
         $result = $tree->fetchAll($tree->select()->where('tree_id = ?', $id));
-        if (! count($result)) {
+        if (!count($result)) {
             throw new Jimw_Admin_Exception("This tree doesn't exist");
         }
         $result = $result->current();
@@ -246,17 +243,16 @@ class TreeController extends Jimw_Admin_Action
         $this->_helper->getHelper('FlashMessenger')->addMessage('Publish successful');
         $this->_forward('list');
     }
-    
+
     /**
      * Archive a page (published but not visible in the menu)
      *
      */
-	public function archiveAction ()
-    {
+    public function archiveAction() {
         $id = $this->_request->id;
         $tree = new Jimw_Site_Tree();
         $result = $tree->fetchAll($tree->select()->where('tree_id = ?', $id));
-        if (! count($result)) {
+        if (!count($result)) {
             throw new Jimw_Admin_Exception("This tree doesn't exist");
         }
         $result = $result->current();
@@ -272,8 +268,7 @@ class TreeController extends Jimw_Admin_Action
      * @param dir Can be "up" or "down" to give the direction of the move
      *
      */
-    public function moveAction ()
-    {
+    public function moveAction() {
         $tree = new Jimw_Site_Tree();
         $save = $tree->find($this->_request->id)->current();
         if ($this->_request->dir == 'down') {
@@ -289,15 +284,14 @@ class TreeController extends Jimw_Admin_Action
         else
             throw new Jimw_Admin_Exception('Illegal move');
         $this->_helper->getHelper('FlashMessenger')->addMessage('Move successful ' . $save->pagetitle);
-        $this->_forward('index','tree', 'module', array('expandAll' => 1)); // When move some page, there are a problem who appear
+        $this->_forward('index', 'tree', 'module', array('expandAll' => 1)); // When move some page, there are a problem who appear
     }
 
     /**
      * Display a popup to choose a name
      *
      */
-    public function chooserAction()
-    {
+    public function chooserAction() {
         if (isset($this->getRequest()->javascript_fct))
             $this->view->javascript_fct = $this->getRequest()->javascript_fct;
         if ($this->getHelper('Layout')->getViewSuffix() == 'ajax') {
@@ -310,11 +304,10 @@ class TreeController extends Jimw_Admin_Action
      * Get a branch of tree
      *
      */
-    public function getAction()
-    {
+    public function getAction() {
         $id = $this->getRequest()->path;
         $tree = new Jimw_Site_Tree();
-        $parentid = $tree->fetchAll($tree->select()->where('tree_parentid = ?', $id)->order(array('tree_lft' , 'tree_alias'))); //$tree->select()->where('tree_parentid = ?', $id)->where('tree_status = ?', Jimw_Site_Tree::PUBLISHED)
+        $parentid = $tree->fetchAll($tree->select()->where('tree_parentid = ?', $id)->order(array('tree_lft', 'tree_alias'))); //$tree->select()->where('tree_parentid = ?', $id)->where('tree_status = ?', Jimw_Site_Tree::PUBLISHED)
         $this->view->list_tree = $parentid;
     }
 
@@ -324,9 +317,7 @@ class TreeController extends Jimw_Admin_Action
      * @param Jimw_Site_Tree_Row $tree
      * @param Jimw_Form $form
      */
-    private function _addModuleConfig(Jimw_Site_Tree_Row $tree, Jimw_Form $form)
-    {
-        $module = $tree->findParentJimw_Site_Module();
+    private function _addModuleConfig(Jimw_Site_Module $module, Jimw_Form $form) {
         if ($module && isset($module->xml->controllers->{$module->path}->params)) {
             $params = $module->xml->controllers->{$module->path};
             $formModule = new Jimw_Form_SubForm($params->params);
@@ -334,5 +325,7 @@ class TreeController extends Jimw_Admin_Action
             $form->addSubForm($formModule, 'tree_param');
         }
     }
+
 }
+
 ?>
