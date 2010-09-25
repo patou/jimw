5bb15fd1c1e64e8ab9562f7a108f8805756f1ed8
patou.de.saint.steban@gmail.com <patou.de.saint.steban@gmail.com@0552e26f-3d1b-0410-805e-4fdf74ef7b9c>
2010-09-25 08:57:56 +0000

    [Jimw Form] Correct UTF-8 language file

--- trunk/jimw/module/form/lang/fr/lang.php
+++ trunk/jimw/module/form/lang/fr/lang.php
@@ -1,12 +1,12 @@
 <?php
-    return array(
-            'Name' => 'Nom',
-    		'Text' => 'Message',
-    		'Your name' => 'Votre nom',
-    		'Send' => 'Envoyer',
-    		'Contact from' => 'Contact sur votre site de ',
-    		'You receive a mail from your website' => 'Vous avez reÁu un message depuis votre site internet',
-    		'This mail is automaticaled send by jimw' => 'Ce mail est envoyÈ automatiquement par Jimw',
-            'Thank you to have submited this form' => 'Merci d\'avoir envoyÈ ce message'
-            
-    );
+
+return array(
+    'Name' => 'Nom',
+    'Text' => 'Message',
+    'Your name' => 'Votre nom',
+    'Send' => 'Envoyer',
+    'Contact from' => 'Contact sur votre site de ',
+    'You receive a mail from your website' => 'Vous avez re√ßu un message depuis votre site internet',
+    'This mail is automatically send by jimw' => 'Ce mail est envoy√© automatiquement par Jimw',
+    'Thank you to have submited this form' => 'Merci d\'avoir envoy√© ce message'
+);
